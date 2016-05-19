73093a399beebf98ee3468fcaa8cd658a5bcd1b5
D. C. Rockwell <dcautomates@gmail.com>
2016-05-19 07:22:12 -0600

    First iteration of a generator generator

---
+++ templates/##generatorName##/generator.js
@@ -0,0 +1,13 @@
+export default class StimpakGenerator {
+	constructor(stimpak) {
+		stimpak
+			.source("**/*")
+				.directory(`${__dirname}/templates`)
+			.prompt({
+				type: "input",
+				name: "templateValueName",
+				message: "What would you do with a drunken sailor?",
+				default: "Tie him to the long boat 'till he's sober"
+			});
+	}
+}
