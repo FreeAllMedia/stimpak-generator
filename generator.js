a5a482c30cfd4ac0c41320c1627f336b8fd0cdcb
D. C. Rockwell <dcautomates@gmail.com>
2016-05-23 08:06:30 -0600

    Added stimpak-readme

--- generator.js
+++ generator.js
@@ -1,25 +1,67 @@
+import packageJson from "./package.json";
+import StimpakReadme from "stimpak-readme";
+
 export default class StimpakGenerator {
 	constructor(stimpak) {
+		stimpak.use(StimpakReadme);
+
+		//this.setTemplateSource(stimpak);
+		this.showLogo(stimpak);
+		//this.askForBasicInformation(stimpak);
+		//this.buildPackages(stimpak);
+
+		//stimpak.quote();
+	}
+
+	setTemplateSource(stimpak) {
 		stimpak
 			.source("**/*")
-				.directory(`${__dirname}/templates`)
-			.prompt({
-				type: "input",
-				name: "generatorName",
-				message: "What do you want your generator to be named?",
-				default: "my-generator"
-			},
-			{
-				type: "input",
-				name: "generatorDescription",
-				message: "How would you describe your generator?",
-				default: "It's a new generator. I don't really have a description for it, yet."
-			})
+				.directory(`${__dirname}/templates`);
+	}
+
+	showLogo(stimpak) {
+		stimpak
+			.logo(`Generator v${packageJson.version}`);
+	}
+
+	askForBasicInformation(stimpak) {
+		stimpak
+			.note("Basic Information:")
+			.prompt(
+				this.generatorNamePrompt,
+				this.generatorDescriptionPrompt
+			);
+	}
+
+	buildPackages(stimpak) {
+		stimpak
 			.then((stimpak, done) => {
-				stimpak.command(`cd ${stimpak.answers().generatorName};npm install`, (stimpak, stdout, stderr, commandDone) => {
+				stimpak.command(`cd stimpak-${stimpak.answers().generatorName};npm install`, (stimpak, stdout, stderr, commandDone) => {
 					commandDone();
 				});
 				done();
-			});
+			})
+	}
+
+	/**
+	 * PROMPTS
+	 */
+
+	get generatorNamePrompt() {
+		return {
+			type: "input",
+			name: "generatorName",
+			message: "What do you want your generator to be named?",
+			default: "my-generator"
+		};
+	}
+
+	get generatorDescriptionPrompt() {
+		return {
+			type: "input",
+			name: "generatorDescription",
+			message: "How would you describe your generator?",
+			default: "It's a new generator. I don't really have a description for it, yet."
+		};
 	}
 }
